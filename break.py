59b97648776dbc30893081e84adece4d360089a4
Shahrokh Abbasi-Rad <shahrokh.zita@gmail.com>
2018-12-31 10:05:18 +1000

    Add dictionary for Enrique

--- break.py
+++ break.py
@@ -33,7 +33,7 @@ riahanna = {'wild thoughts':'https://www.youtube.com/watch?v=fyaI4-5849w',
 "Work (Explicit) feat Drake":'https://www.youtube.com/watch?v=m2hI1_UBN2c',
 'diamonds':"https://www.youtube.com/watch?v=lWA2pjMjpBs"}
 
-britney = {'circus':"https://www.youtube.com/watch?v=lVhJ_A8XUgc",
+britney_spears = {'circus':"https://www.youtube.com/watch?v=lVhJ_A8XUgc",
 'pretty girls':"https://www.youtube.com/watch?v=uV2uebhnqOw",
 'till the world ends':"https://www.youtube.com/watch?v=qzU9OrZlKb8",
 'oops I did it again':"https://www.youtube.com/watch?v=CduA0TULnow",
@@ -44,9 +44,23 @@ britney = {'circus':"https://www.youtube.com/watch?v=lVhJ_A8XUgc",
 'baby one more time':'https://www.youtube.com/watch?v=C-u5WLJ9Yk4',
 'scream and shout':'https://www.youtube.com/watch?v=kYtGl1dX5qI'}
 
-singers = ('Michael Jackson','Shakira', 'Rihanna')
+enrique_iglesias = {'hero':'https://www.youtube.com/watch?v=koJlIGDImiU',
+'no me digas que no':'https://www.youtube.com/watch?v=zyqt2avPkoA',
+'duelle el corazon':'https://www.youtube.com/watch?v=xFutjZEBTXs',
+'loco':'https://www.youtube.com/watch?v=RSyUWjftHrs',
+'coando me enamoro':'https://www.youtube.com/watch?v=4DO8GsIYfhQ',
+'el perdedor':'https://www.youtube.com/watch?v=tLcfAnN2QgY',
+'I like how it feels':'https://www.youtube.com/watch?v=klk8QPcfFYQ',
+"I'm a freak":'https://www.youtube.com/watch?v=YUiVIPgJA0o',
+'Not in love':'https://www.youtube.com/watch?v=njKo3l0MKQ0',
+'Love to see you cry':'https://www.youtube.com/watch?v=LWdwkdYPz-o',
+'heartbeat':'https://www.youtube.com/watch?v=NVk4vENObiI',
+'Addicted':'https://www.youtube.com/watch?v=2M-2BFS6Jxc',
+'Rythm divine':'https://www.youtube.com/watch?v=UZrzOZaI7UE'}
 
-print('From which one do you want to watch? \n1.Michael Jackson\n2.Shakira\n3.Rihnna')
+singers = ('Michael Jackson','Shakira', 'Rihanna', 'Britney', 'Enrique')
+
+print('From which one do you want to watch? \n1.Michael Jackson\n2.Shakira\n3.Rihnna\n4.Britney\n5.Enrique')
 while True:
     selected_singer = input()
     if selected_singer.isdigit() and 1 <= int(selected_singer) <= len(singers):
